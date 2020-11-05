6edc21715ccec5c88d6945e78b12c483282564d7
Martin Schweinberger <martin.schweinberger.hh@gmail.com>
2020-11-05 19:51:46 +1000

    dutch update

---
+++ R/dutch-data.R
@@ -0,0 +1,16 @@
+#' Example data representing a Dutch sentence
+#'
+#' Example data representing a Dutch sentence which defines linguistics.
+#'
+#' @docType data
+#'
+#' @usage data(dutch)
+#'
+#' @format A single character string consisting of 107 character tokens.
+#' 
+#' @keywords datasets
+#'
+#' @examples
+#' data(dutch)
+#' \donttest{nchar(dutch)}
+"dutch"
